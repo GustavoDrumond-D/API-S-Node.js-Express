5a88a9d92f4bf05ed990cbd578a8e974ea4a865e
Gustavo Drumond <gdrumondduarte@gmail.com>
2025-01-17 19:57:27 -0300

    Criando rota PUT

--- CookBook_API/src/rotas/app.js
+++ CookBook_API/src/rotas/app.js
@@ -21,9 +21,24 @@ app.get('/receitas', (req, res) => {
     res.status(200).send(receitas)
 })
 
+app.get('/receitas/:id', (req, res) => {
+    const receita = receitas.findIndex(receita => {
+        return receita.id == Number(req.params.id)
+    })
+    res.status(200).send(receitas[receita])
+})
+
 app.post('/receitas', (req, res) => {
     receitas.push(req.body)
     res.status(201).json("receita criada com sucesso")
+})
+
+app.put('/receitas/:id', (req, res) => {
+    const receita = receitas.findIndex(receita => {
+        return receita.id == Number(req.params.id)
+    })
+    receitas[receita].titulo = req.body.titulo
+    res.status(200).json("receita atualizada com sucesso")
 })
 
 export default app;
