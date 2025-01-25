dfec81203febcec12b2953bd40d62f107885905b
Gustavo Drumond <gdrumondduarte@gmail.com>
2025-01-25 11:42:52 -0300

    Arrumando arquivo server.js

--- Projeto_Alura/server.js
+++ Projeto_Alura/server.js
@@ -1,3 +1,4 @@
+import "dotenv/config";
 import app from "./src/app.js";
 
 const porta = 3000
@@ -5,3 +6,5 @@ const porta = 3000
 app.listen(porta, () => {
     console.log("Servidor rodando na porta 3000");
 })
+
+// mongodb+srv://admin:<db_password>@cluster0.3ciir.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
