56fadead81bf277a53e57e3ae5f68d66a009c0b9
Gustavo Drumond <gdrumondduarte@gmail.com>
2025-02-04 18:14:38 -0300

    Adicionando numero minimo e maximo de páginas.

--- Projeto_Alura_2.0/src/models/Livro.js
+++ Projeto_Alura_2.0/src/models/Livro.js
@@ -14,9 +14,22 @@ const livroSchema = new mongoose.Schema(
     },
     editora: {
       type: String,
-      required: [true, "Editora é obrigatoria"]
+      required: [true, "Editora é obrigatoria"],
+      
+      // O campo "enum" define um conjunto fixo de valores permitidos para a propriedade correspondente. 
+      // Isso garante que os dados sigam um padrão predefinido e evita valores inesperados ou inválidos. 
+      // No exemplo abaixo, o array está vazio, o que significa que ainda não há restrições definidas.
+      // Neste projeto, vou deixar vazio.
+      // enum: {
+      //   values:[],
+      //   message: "A editora {VALUE} não é um valor permitido."
+      // }
     },
-    numeroPaginas: { type: Number }
+    numeroPaginas: { 
+      type: Number,
+      min: [10, "O número de páginas deve esta de 10 a 5000. Valor fornecido: {VALUE}"],
+      max: [5000, "O número de páginas deve esta de 10 a 5000. Valor fornecido: {VALUE}"]
+    }
   }
 );
 
