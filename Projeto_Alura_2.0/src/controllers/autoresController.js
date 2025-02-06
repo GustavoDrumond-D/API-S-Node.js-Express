ece7b56eae5316f82518e4b61b96d53b36e76a86
Gustavo Drumond <gdrumondduarte@gmail.com>
2025-02-06 11:20:16 -0300

    atualizando controlador dos autores, comentando código

--- Projeto_Alura_2.0/src/controllers/autoresController.js
+++ Projeto_Alura_2.0/src/controllers/autoresController.js
@@ -3,8 +3,10 @@ import {autores} from "../models/index.js";
 
 class AutorController {
 
+  // Listar todos os autores
   static listarAutores = async (req, res) => {
     try {
+      // autores.find() retorna uma promise
       const autoresResultado = await autores.find();
 
       res.status(200).json(autoresResultado);
@@ -14,13 +16,16 @@ class AutorController {
     }
   };
 
+  // Listar autor por id
   static listarAutorPorId = async (req, res, next) => {
 
     try {
+      // vai pegar o id do autor a ser buscado
       const id = req.params.id;
-
+      // vai buscar o autor pelo id
       const autorResultado = await autores.findById(id);
 
+      // se o autor for encontrado ele vai ser retornado, caso contrario vai ser retornado um erro
       if (autorResultado !== null) {
         res.status(200).send(autorResultado);
       } else {
@@ -31,10 +36,13 @@ class AutorController {
     }
   };
 
+  // Cadastrar autor
   static cadastrarAutor = async (req, res, next) => {
     try {
+      // vai criar um novo autor
       let autor = new autores(req.body);
 
+      // vai salvar o autor
       const autorResultado = await autor.save();
 
       res.status(201).send(autorResultado.toJSON());
@@ -43,12 +51,15 @@ class AutorController {
     }
   };
 
+  // Atualizar autor
   static atualizarAutor = async (req, res, next) => {
     try {
       const id = req.params.id;
 
+      // vai atualizar o autor pelo id
       const autoresResultado = await autores.findByIdAndUpdate(id, { $set: req.body });
 
+      // se o autor for encontrado ele vai ser retornado, caso contrario vai ser retornado um erro
       if (autoresResultado !== null){
         res.status(200).send({message: "Autor atualizado com sucesso"});
       } else {
@@ -59,12 +70,15 @@ class AutorController {
     }
   };
 
+  // Excluir autor
   static excluirAutor = async (req, res, next) => {
     try {
       const id = req.params.id;
 
+      // vai excluir o autor pelo id
       const autoresResultado = await autores.findByIdAndDelete(id);
       
+      // se o autor for encontrado ele vai ser retornado, caso contrario vai ser retornado um erro
       if (autoresResultado !== null){
         res.status(200).send({ message: "Autor removido com sucesso" });
       } else {
